2d3d9dfc51828a8ce8ff3347e294db8617d8b5fa
Dean <1602264241@qq.com>
2023-07-05 16:43:08 +0800

    修复撤单后可能语音不提醒的bug

--- THS/Voice.py
+++ THS/Voice.py
@@ -5,13 +5,13 @@ import pyttsx4
 
 class Voice:
     def __init__(self):
-        self.engine = pyttsx4.init()
         self.play_lock = threading.Lock()
 
     def _say(self, text):
         with self.play_lock:
-            self.engine.say(text)
-            self.engine.runAndWait()
+            engine = pyttsx4.init()
+            engine.say(text)
+            engine.runAndWait()
 
     def say(self, text):
         threading.Thread(target=self._say, args=(text,)).start()
